7677483caf52a68e94fded27cf5b3baec13dbb08
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2015-06-02 16:26:45 +1000

    add options processing

--- src/Options.hs
+++ src/Options.hs
@@ -1,19 +1,35 @@
+{-# LANGUAGE TemplateHaskell #-}
 
-module Options where
+module Options
+  where
+
+import Backend
+
+import Data.List                                ( intercalate )
+import Control.Lens
+import System.Exit
+import System.Environment
+import System.Console.GetOpt
+import Text.PrettyPrint.ANSI.Leijen
+
+import Data.Array.Accelerate.Debug
 
 
 data Options = Options
   {
-    optMaxSteps         :: Int                  -- maximum number of cycles to run
-  , optSize             :: Int                  -- length of cube mesh along each dimension
-  , optNumRegions       :: Int                  -- number of distinct regions
-  , optBalance          :: Int                  -- load balance between regions of a domain
-  , optCost             :: Int                  -- extra cost of more expensive regions
-  , optOutputFile       :: Maybe FilePath       -- output viz files
-  , optProgress         :: Bool                 -- print out progress
-  , optHelp             :: Bool                 -- print help message
+    _optBackend         :: Backend              -- backend to execute with
+  , _optMaxSteps        :: Int                  -- maximum number of cycles to run
+  , _optSize            :: Int                  -- length of cube mesh along each dimension
+--  , _optNumRegions      :: Int                  -- number of distinct regions
+--  , _optBalance         :: Int                  -- load balance between regions of a domain
+--  , _optCost            :: Int                  -- extra cost of more expensive regions
+  , _optOutputFile      :: Maybe FilePath       -- output viz files
+  , _optProgress        :: Bool                 -- print out progress
+  , _optHelp            :: Bool                 -- print help message
   }
   deriving (Eq, Show)
+
+$(makeLenses ''Options)
 
 
 -- The size of the simulation box
@@ -26,19 +42,120 @@ _DEPTH  = 1.125
 
 -- Default simulation options
 --
-options :: Options
-options = Options
+defaultOptions :: Options
+defaultOptions = Options
   {
-    optMaxSteps         = 9999999
-  , optSize             = 30
-  , optNumRegions       = 11
-  , optBalance          = 1
-  , optCost             = 1
-  , optOutputFile       = Nothing
-  , optProgress         = False
-  , optHelp             = False
+    _optBackend         = defaultBackend
+  , _optMaxSteps        = 9999999
+  , _optSize            = 30
+--  , _optNumRegions      = 11
+--  , _optBalance         = 1
+--  , _optCost            = 1
+  , _optOutputFile      = Nothing
+  , _optProgress        = False
+  , _optHelp            = False
   }
 
-parseOptions :: [String] -> Options
-parseOptions _ = options
+
+options :: [OptDescr (Options -> Options)]
+options =
+  availableBackends optBackend ++
+  [
+    Option  "n" ["size"]
+            (ReqArg (set optSize . read) "INT")
+            "problem size in each dimension"
+
+  , Option  []  ["max-steps"]
+            (ReqArg (set optMaxSteps . read) "INT")
+            "maximum number of iterations to run"
+
+  , Option  []  ["output"]
+            (ReqArg (set optOutputFile . Just) "FILE")
+            "path to output VisIt files"
+
+  , Option  []  ["progress"]
+            (NoArg (set optProgress True))
+            "print simulation progress"
+
+  , Option  "h?" ["help"]
+            (NoArg (set optHelp True))
+            "show help message"
+  ]
+
+
+-- | Format a (console) string as bold text. Assume the user has configured
+-- their terminal colours to something that looks good (and avoids the light vs.
+-- dark background debate).
+--
+title :: String -> String
+title = show . bold . text
+
+
+-- | Generate the list of available (and the selected) Accelerate backends.
+--
+fancyHeader :: Options -> [String] -> [String] -> String
+fancyHeader opts hdr ftr = intercalate "\n" (hdr ++ body ++ ftr)
+  where
+    active this         = if this == show (view optBackend opts) then "*" else ""
+    (ss,bs,ds)          = unzip3 $ map (\(b,d) -> (active b, b, d)) $ concatMap extract (availableBackends optBackend)
+    table               = zipWith3 paste (sameLen ss) (sameLen bs) ds
+    paste x y z         = "  " ++ x ++ "  " ++ y ++ "  " ++ z
+    sameLen xs          = flushLeft ((maximum . map length) xs) xs
+    flushLeft n xs      = [ take n (x ++ repeat ' ') | x <- xs ]
+    --
+    extract (Option _ los _ descr) =
+      let losFmt  = intercalate ", " los
+      in  case lines descr of
+            []          -> [(losFmt, "")]
+            (x:xs)      -> (losFmt, x) : [ ("",x') | x' <- xs ]
+    --
+    body                = title "Available backends:"
+                        : table
+
+
+-- | Parse the command line arguments and return the resulting options
+-- structure, together with any unrecognised or unprocessed options.
+--
+-- Anything following a "--" is not processed.
+--
+parseArgs :: IO (Options, [String])
+parseArgs = do
+  accInit
+  args <- getArgs
+
+  let
+      (argv, rest) =
+        let (x,y) = span (/= "--") args
+        in  (x, dropWhile (== "--") y)
+
+      helpMsg []  = helpMsg'
+      helpMsg err = unlines [ concat err, helpMsg' ]
+
+      helpMsg'    = unlines
+        [ fancyHeader defaultOptions header []
+        , ""
+        , usageInfo (title "Options:") options
+        ]
+
+      (opts, nonopts) = case getOpt' Permute options argv of
+          (o,n,u,[])  -> (foldr id defaultOptions o, n ++ u)
+          (_,_,_,err) -> error (helpMsg err)
+
+  if view optHelp opts
+     then putStr   (helpMsg []) >> exitSuccess
+     else putStrLn (fancyHeader opts header footer)
+
+  return (opts, nonopts ++ rest)
+
+
+header, footer :: [String]
+header =
+  [ "accelerate-lulesh (c) [2015] The Accelerate Team"
+  , ""
+  , "Usage: accelerate-lulesh [OPTIONS]"
+  , ""
+  ]
+
+footer =
+  [ "" ]
 
