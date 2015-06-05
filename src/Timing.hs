c6c88d826106c25da53c75cb21b47c2622511ea8
Trevor L. McDonell <tmcdonell@cse.unsw.edu.au>
2015-06-06 00:51:51 +1000

    add some timing utilities

---
+++ src/Timing.hs
@@ -0,0 +1,88 @@
+{-# LANGUAGE BangPatterns #-}
+--
+-- Timing utilities
+--
+module Timing where
+
+import Numeric
+import Data.Time                                        hiding ( DiffTime )
+import System.CPUTime
+import Control.Applicative
+import Prelude
+
+
+-- Time -----------------------------------------------------------------------
+-- | Abstract representation of process time.
+--
+data Time = Time
+  { cpuTime     :: !Integer
+  , wallTime    :: !UTCTime
+  }
+  deriving Show
+
+data DiffTime = DiffTime !Integer !NominalDiffTime
+
+instance Show DiffTime where
+  show (DiffTime cpu wall) =
+    "wall: " ++ showFFloatSIBase (Just 3) 1000 (fromRational (toRational wall) :: Double) "s, " ++
+    "cpu: "  ++ showFFloatSIBase (Just 3) 1000 (fromIntegral cpu * 1.0e-12 :: Double) "s"
+
+-- | Get the current time
+--
+getTime :: IO Time
+getTime =
+  Time <$> getCPUTime <*> getCurrentTime
+
+
+-- | Difference of two times
+--
+diffTime :: Time -> Time -> DiffTime
+diffTime (Time c1 w1) (Time c2 w2) = DiffTime (c1-c2) (diffUTCTime w1 w2)
+
+
+-- Timing operations ----------------------------------------------------------
+
+-- | Time some IO action.
+--
+--   Make sure to deepseq the result before returning it from the action. If you
+--   don't do this then there's a good chance that you'll just pass a suspension
+--   out of the action, and the computation time will be zero.
+--
+time :: IO a -> IO (a, DiffTime)
+{-# NOINLINE time #-}
+time it = do
+  start <- getTime
+  !r    <- it
+  end   <- getTime
+  return (r, diffTime end start)
+
+
+-- | Show a signed 'RealFloat' value using SI unit prefixes. In the call to:
+--
+-- > showFFloatSIBase prec base val
+--
+-- If @prec@ is @'Nothing'@ the value is shown to full precision, and if @prec@
+-- is @'Just' d@, then at most @d@ digits are shown after the decimal place.
+-- Here @base@ represents the increment size between multiples of the original
+-- unit. For measures in base-10 this will be 1000 and for values in base-2 this
+-- is usually 1024, for example when measuring seconds versus bytes,
+-- respectively.
+--
+showFFloatSIBase :: RealFloat a => Maybe Int -> a -> a -> ShowS
+showFFloatSIBase prec !base !k
+  = showString
+  $ case pow of
+      4   -> with "T"
+      3   -> with "G"
+      2   -> with "M"
+      1   -> with "k"
+      -1  -> with "m"
+      -2  -> with "µ"
+      -3  -> with "n"
+      -4  -> with "p"
+      _   -> showGFloat prec k " "      -- no unit or unhandled SI prefix
+  where
+    !k'         = k / (base ^^ pow)
+    !pow        = floor (logBase base k) :: Int
+    with unit   = showFFloat prec k' (' ':unit)
+
